c427a6b00713a3ab1982ef09a45990087777c8b9
DerEns86 <v.ens86@yahoo.de>
2025-03-20 15:59:27 +0100

    feat: add user profile route and implement UserProfileView to display user-specific posts

--- client/src/views/UserProfileView.vue
+++ client/src/views/UserProfileView.vue
@@ -1,5 +1,25 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { usePostsStore } from '@/stores/posts'
+import PostList from '@/components/PostList.vue'
+import { useRoute } from 'vue-router'
 
-<template></template>
+const route = useRoute()
+const id = route.params.userId
+
+const postsStore = usePostsStore()
+
+onMounted(async () => {
+  await postsStore.fetchPostsByUserId(id)
+})
+</script>
+
+<template>
+  <button @click="$router.go(-1)">Back</button>
+  <div>
+    <h2>My Posts</h2>
+    <PostList :posts="postsStore.userPosts" />
+  </div>
+</template>
 
 <style scoped></style>
