8b07f2012818a3f0204c6f8dd92acd6a3460d156
DerEns86 <v.ens86@yahoo.de>
2025-03-20 15:32:08 +0100

    feat: add fetchPostsByUserId function to posts store and update MyProfileView to display user-specific posts

--- client/src/stores/posts.ts
+++ client/src/stores/posts.ts
@@ -5,10 +5,17 @@ import type { PostInterface } from '@/interfaces/post.interface'
 export const usePostsStore = defineStore('posts', () => {
   const API_URL = 'http://localhost:3000/'
   const posts = ref<PostInterface[]>([])
+  const userPosts = ref<PostInterface[]>([])
 
   async function fetchPosts() {
     const response = await fetch(`${API_URL}posts`)
     posts.value = await response.json()
+  }
+
+  async function fetchPostsByUserId(userId: string) {
+    const response = await fetch(`${API_URL}users/${userId}?_embed=posts`)
+    const data = await response.json()
+    userPosts.value = data.posts
   }
 
   function likePost(postId: string) {
@@ -33,5 +40,5 @@ export const usePostsStore = defineStore('posts', () => {
 
   const likedPosts = computed(() => posts.value.filter((post) => post.isLiked))
 
-  return { posts, fetchPosts, likePost, unlikePost, likedPosts }
+  return { posts, fetchPosts, likePost, unlikePost, likedPosts, fetchPostsByUserId, userPosts }
 })
--- client/src/views/MyProfileView.vue
+++ client/src/views/MyProfileView.vue
@@ -1,20 +1,27 @@
 <script setup>
 import { onMounted } from 'vue'
 import { usePostsStore } from '@/stores/posts'
+import { useUserStore } from '@/stores/user'
 import PostList from '@/components/PostList.vue'
 
 const postsStore = usePostsStore()
+const userStore = useUserStore()
 
-// onMounted(async () => {
-//   await postsStore.likedPosts
-//   console.log(postsStore.likedPosts)
-// })
+onMounted(async () => {
+  await postsStore.fetchPostsByUserId(userStore.currentUser.id)
+})
 </script>
 
 <template>
-  <PostList :posts="postsStore.likedPosts" />
+  <div>
+    <h2>My Posts</h2>
+    <PostList :posts="postsStore.userPosts" />
+  </div>
 
-  <!-- <p v-for="post of postsStore.likedPosts" :key="post">test</p> -->
+  <div>
+    <h2>My liked Posts</h2>
+    <PostList :posts="postsStore.likedPosts" />
+  </div>
 </template>
 
 <style scoped></style>
